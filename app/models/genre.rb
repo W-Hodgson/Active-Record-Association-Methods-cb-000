f5571eaead2823fe445362ed36cbb460f954f186
Will Hodgson <will.hodgson@current-rms.com>
2019-10-10 13:26:19 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,7 +9,7 @@ class Genre < ActiveRecord::Base
   def artist_count
     artists = []
     self.songs.all.each {|song| artists << song.artist}
-    artists.uniq
+    artists.uniq.size
   end
 
   def all_artist_names
